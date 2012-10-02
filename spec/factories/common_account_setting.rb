c5878ead841d1b7463056f3410f87ba62c85f76f
Seth Vargo <sethvargo@gmail.com>
2012-10-02 15:27:53 -0400

    fix failing common_account_setting_spec

--- spec/factories/common_account_setting.rb
+++ spec/factories/common_account_setting.rb
@@ -2,14 +2,14 @@ FactoryGirl.define do
   factory :common_account_setting do
     name 'AOL'
     incoming_server {{
-      address: 'imap.example.com',
-      port: 993,
-      ssl: true,
+      'address' => 'imap.example.com',
+      'port' => 993,
+      'ssl' => true,
     }}
     outgoing_server {{
-      address: 'smtp.example.com',
-      port: 587,
-      ssl: true,
+      'address' => 'smtp.example.com',
+      'port' => 587,
+      'ssl' => true,
     }}
   end
 end
