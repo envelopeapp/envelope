a8502a48056834e6186006b21aa6771381450557
Seth Vargo <sethvargo@gmail.com>
2012-11-24 23:35:19 -0500

    remove failing contact_spec
    - It wasn't actually doing anything

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -12,20 +12,6 @@ describe Contact do
   # validations
 
   # methods
-  describe '.import_from_vcard' do
-    it 'creates the temporary directory' do
-      tmp = Rails.root.join('tmp', 'uploads')
-      FileUtils.rm_rf(tmp)
-      lambda { Dir::mkdir(tmp) }.should_not raise_error
-    end
-
-    it 'builds a unique temporary path to tmp' do
-      path = [Rails.root.join('tmp', 'uploads'), "#{Time.now.to_i.to_s}.vcf"].join('/')
-      lambda { File.open(path,  'w+') { |f| f.write('test') } }.should_not raise_error
-      FileUtils.rm_rf(path)
-    end
-  end
-
   its(:name){ should == 'Bill Jones' }
   its(:first_name){ should == 'Bill' }
   its(:last_name){ should == 'Jones' }
