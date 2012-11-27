f779226d42dcb086370bb65f4f10925ca7202a85
Elizabeth Li <etli@andrew.cmu.edu>
2012-11-27 13:18:06 -0500

    Added jQuery animations, screenshots page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
   
+  // if localStorage is supported, intro animation only plays on first visit
+  if (localStorage.visited == true || localStorage.visited == "true") {
+    $('#inv').remove();
+  }
+  
+  if (!localStorage.visited) {
+    // intro animation
+    $('#slogan').delay(1000).fadeIn(1000, function() {
+      $('.navbar, #three-col, #footer').delay(1000).fadeTo(1000, 1);
+      localStorage.visited = true;
+    });
+  }
+  
+  $('#three-col .span4').hover(function() {
+    $(this).animate({ 'opacity': '1' });
+    $(this).children('.caption').animate({ 'padding-bottom': '30px' });
+  }, function() {
+     $(this).animate({ 'opacity': '0.9' });
+    $(this).children('.caption').animate({ 'padding-bottom': '15px' });
+  });
+  
 });
